3142e886b62c6921fdd18cd5acbd76efac7ad98d
Putnug1122 <asiapatta10@gmail.com>
2022-05-19 20:37:07 +0800

    init frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,53 @@
+import { ErrorInterceptorService } from './_interceptors/error-interceptor.service';
+import { NavigationComponent } from './parts/navigation/navigation.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { CardComponent } from './pages/card/card.component';
+import { CartComponent } from './pages/cart/cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { OrderComponent } from './pages/order/order.component';
+import { OrderDetailComponent } from './pages/order-detail/order-detail.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { UserEditComponent } from './pages/user-edit/user-edit.component';
+import { PaginationComponent } from './parts/pagination/pagination.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { JwtInterceptorService } from './_interceptors/jwt-interceptor.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    CartComponent,
+    LoginComponent,
+    OrderComponent,
+    OrderDetailComponent,
+    ProductDetailComponent,
+    ProductListComponent,
+    SignupComponent,
+    UserEditComponent,
+    NavigationComponent,
+    PaginationComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  providers: [
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptorService,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptorService,
+      multi: true,
+    },
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
