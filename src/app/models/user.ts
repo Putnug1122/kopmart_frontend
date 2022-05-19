0824cd3c957fcf531a37f79603e8904ecbd44d74
Putnug1122 <asiapatta10@gmail.com>
2022-05-19 21:35:29 +0800

    model and enum

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,2 +1,22 @@
+import { Role } from '../enum/role';
+
 export class User {
+  email: string;
+
+  password: string;
+
+  name: string;
+
+  phone: string;
+
+  address: string;
+
+  active: boolean;
+
+  role: string;
+
+  constructor() {
+    this.active = true;
+    this.role = Role.Customer;
+  }
 }
